d76fd2fedb7d04d82d56a3af250abfe7e524d042
Your Vineetha <vineethadandu111@gmail.com>
2019-09-25 15:16:55 -0500

    this is commit for main controller file change

--- src/main/java/com/vineethagloo/demospringboot/controllers/MainController.java
+++ src/main/java/com/vineethagloo/demospringboot/controllers/MainController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+//this is HelloWorld controller
 @Controller
 public class MainController {
 	
